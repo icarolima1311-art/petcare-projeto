ef3a370d648d16359cc102c64b1c4e7c4432d673
Icaro Oliveira <icarolima1311@gmail.com>
2025-11-22 19:56:10 -0300

    Versão Final: Projeto Completo para Deploy

---
+++ config.js
@@ -0,0 +1,9 @@
+// --- ARQUIVO SECRETO DE CONFIGURAÇÃO ---
+// (Este arquivo será ignorado pelo Git)
+
+// 1. Cole a URL do seu Projeto Supabase aqui
+const SUPABASE_URL = 'https://ssmhsdnlqarxgmhxfzqw.supabase.co'; 
+
+// 2. Cole a sua Chave 'anon public' aqui
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzbWhzZG5scWFyeGdtaHhmenF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5OTMxMTcsImV4cCI6MjA3NzU2OTExN30.esQmoUG3ZVD6lZ5_O29kUNK4IzHq4EAvaTUfcjBQ8kE';
+
